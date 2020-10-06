17723cebda21edf308dd806982a354929c629f8d
Pradhvan <pradhvanbisht@gmail.com>
2020-10-06 22:12:32 +0530

    Day1: Hello World + Failing Tests

---
+++ hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, " 
+
+func Hello(name string) string {
+	if name == "" {
+		name = "world"
+	}
+	return englishHelloPrefix + name + "!"
+}
+
+func main() {
+	fmt.Println(Hello("Max"))
+}
